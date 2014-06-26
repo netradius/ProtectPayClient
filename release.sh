afa8d68b08b11999606432b262ae0c1ad44db5a3
Erik R. Jensen <ejensen@netradius.com>
2014-06-26 17:11:28 -0600

    Updating release script.

--- release.sh
+++ release.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+echo "Set the next RELEASE version"
 mvn versions:set
 mvn versions:commit
 git commit pom.xml -m "Incrementing version"
 mvn deploy
 mvn scm:tag
+
+echo "Set the next SNAPSHOT version"
+mvn versions:set
+mvn versions:commit
+git commit pom.xml -m "Incrementing version"
+git push
